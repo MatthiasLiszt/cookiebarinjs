ad28c902265e7cfd1fd4d9e0476485287e9ba5ea
MatthiasLiszt <mliszt.program@gmail.com>
2017-10-31 15:58:15 +0100

    tested with firefox

--- cookiebar.js
+++ cookiebar.js
@@ -1,17 +1,22 @@
 
 var boringLaw="some boring EU cookie law";
 var u=new Date();
-var cookieText="qookiebar="+u.getTime();
+var cookieName="qookiebar=";
+var cookieText=cookieName+u.getTime();
 
 console.log(cookieText);
 
 var qookie=document.getElementById('qookiebar');
-var Accept="<button class='qookiebutton' onclick='(function(){document.cookie=\""+cookieText+"\"})();'>Accept</button>";
+var Accept="<button class='qookiebutton' onclick='(function(){document.cookie=\""+cookieText+"\";document.getElementById(\"qookiebar\").style.display=\"none\";})();'>Accept</button>";
 var MoreInfo="<button class='qookiebutton' onclick='alert(\""+boringLaw+"\")'>MoreInfo</button>";
 
-qookie.innerHTML="This site contains cookies."+Accept+MoreInfo;
-
-qookiefocus();
+console.log("cookie found at "+document.cookie.indexOf(cookieName));
+if(document.cookie.indexOf(cookieName)<0)
+ {qookie.innerHTML="This site contains cookies."+Accept+MoreInfo;
+  qookiefocus();
+ }  
+else
+ {qookie.style.display="none";} 
 
 function qookiefocus(){
  var qookie=document.getElementById('qookiebar').focus();
